4e566ead595b3202f38d1f240d1ea9a1dfa0e0c7
Money-Ken <noopassgamer@gmail.com>
2025-05-14 17:14:11 +0400

    qwe

---
+++ Social.Domain/Model/UserGroupRole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social.Domain.Model;
+/// <summary>
+/// Класс, описывающий роли пользователей в группе.
+/// </summary>
+public class UserGroupRole
+{
+    /// <summary>
+    /// Индификатор пользователя.
+    /// </summary>
+    public int UserId { get; set; }
+
+    /// <summary>
+    /// Индификатор группы.
+    /// </summary>
+    public int GroupId { get; set; }
+
+    /// <summary>
+    /// Роль пользователя в группе.
+    /// </summary>
+    public UserRole Role { get; set; }
+}
